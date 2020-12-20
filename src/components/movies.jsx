5a69e39c417c8b66030ce0d810ccf34b08d9afce
Gharbi <leithgharbi@yahoo.fr>
2020-12-20 10:33:54 +0100

    18- Sorting- Moving Responsibility

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -57,6 +57,7 @@ class Movies extends Component {
               <p>Showing {filtered.length} movies in the database.</p>
               <MoviesTable
                 movies={movies}
+                sortColumn={sortColumn}
                 onLike={this.handleLike}
                 onDelete={this.handleDelete}
                 onSort={this.handleSort}
@@ -99,16 +100,10 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
-    else {
-      sortColumn.path = path;
-      sortColumn.order = 'asc';
-    }
+  handleSort = (sortColumn) => {
+
     this.setState({ sortColumn });
-    console.log(path);
+  
   };
 }
 
